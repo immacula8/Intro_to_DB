30d07cb0f683d2280e27d7d8d499a7155897ff36
Immaculate <geoffreyimmaculate@gmail.com>
2025-06-28 22:23:40 +0100

    Add task_2.sql for table creation

--- task_2.sql
+++ task_2.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE IF NOT EXISTS alx_book_store;
 USE alx_book_store;
 
-CREATE TABLE IF NOT EXISTS authors (
+CREATE TABLE Authors (
     author_id INT AUTO_INCREMENT PRIMARY KEY,
     author_name VARCHAR(215)
 );
